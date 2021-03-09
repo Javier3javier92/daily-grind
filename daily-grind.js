82ffcaf1e3178988d7a6cfbd2772a6a29a8a609d
Javier Mejia <javier3javier92@gmail.com>
2021-03-08 20:07:41 -0800

    changes

--- daily-grind.js
+++ daily-grind.js
@@ -26,6 +26,8 @@ function coffeeTemplate(coffee){
     myReturn +=
 
     `<p>
+    <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee"/>
+    <strong class="feature">${coffee.day}'s Coffee Special:</strong><br> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>${coffee.desc}
 
     </p>
     `
@@ -41,29 +43,38 @@ function coffeeTemplate(coffee){
 
 switch (myDay) {
 
-
+    case 0:
+        today = "Sunday";
+        coffee = {
+            name:"Pumpkin spice latte",
+            pic:"images/pumpkin-spice-latte.jpg",
+            alt: "A picture of a Drip pumpkin spice latte",
+            day:"Sunday",
+            color:"white",
+            desc: "pumkin spice latte"
+        };
+    break;
     case 1:
     today = "Monday";
     coffee = {
         name:"Bubble Tea",
         pic:"images/bubble-tea.jpg",
-        alt: "A picture of a Drip coffee",
-        day:"Wednesday",
-        desc:"pink"
-
+        alt: "A picture of a Bubble tea",
+        day:"Monday",
+        color:"pink",
+        desc: "bubble tea"
     };
 break;
 
     case 2:
         today = "Tuesday";
         coffee = {
-            color: "brown",
             name:"Drip",
             pic:"images/drip.jpg",
             alt: "A picture of a Drip coffee",
-            day:"Wednesday",
-            desc:brown
-
+            day:"Tuesday",
+            color"brown",
+            desc: "drip"
         };
     break;
 
@@ -74,24 +85,48 @@ break;
             name:"Caramel",
             pic:"images/caramel-latte.jpg",
             alt: "A picture of a caramel latte",
-            day:"Wednesday",
-            desc:"yellow"
+            day:"wednesday",
+            color:"yellow",
+            desc: "caramel latte"
 
         };
     break;
 
     case 4:
-    today = "Monday";
+    today = "Thursday";
     coffee = {
-        name:"Bubble Tea",
-        pic:"images/bubble-tea.jpg",
-        alt: "A picture of a Drip coffee",
-        day:"Wednesday",
-        desc:"brown"
+        name:"cold brew",
+        pic:"images/cold-brew.jpg",
+        alt: "A picture of a cold brew",
+        day:"Thursday",
+        color:"red",
+        desc: "cold brew"
 
     };
 break;
+case 5:
+    today = "Friday";
+    coffee = {
+        name:"frappaccino",
+        pic:"images/frappaccino.jpg",
+        alt: "A picture of a frappaccino",
+        day:"Friday",
+        color:"blue",
+        desc: "frappaccino"
 
+    };
+break;
+case 6:
+    today = "Saturday";
+    coffee = {
+        name:"Mocha",
+        pic:"images/mocha.jpg",
+        alt: "A picture of a mocha",
+        day:"Saturday",
+      color:"green",
+      desc: "mocha"
+    };
+break;
 
 
 }
